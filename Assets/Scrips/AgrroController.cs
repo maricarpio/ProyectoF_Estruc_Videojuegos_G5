87c3c851a252506beeda11ae25b3a5a593732511
maricarpio <maripacarpio@gmail.com>
2023-08-21 00:46:16 -0600

    Cambio a aggro

--- Assets/Scrips/AgrroController.cs
+++ Assets/Scrips/AgrroController.cs
@@ -1,67 +1,40 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AgrroController : MonoBehaviour
 {
-    [SerializeField]
-    Transform player;
-
-    [SerializeField]
-    float agroRange;
-
-    [SerializeField]
-    float moveSpeed;
-
-    Rigidbody2D rb2d;
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+    private float distance;
 
 
-    void Start()
+    void Update()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
 
-    }
 
-    private void Update()
-    {
-        float distToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if (distToPlayer > agroRange)
+        if (distance < distanceBetween)
         {
-            ChasePlayer();
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
 
+            
+            if (direction.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            
+            else if (direction.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1); 
+            }
         }
-        else 
-        {
-            StopChasingPlayer();
-        }
-
-
-
-    }
-
-    void ChasePlayer()
-    {
-        if (transform.position.x < player.position.x)
-        {
-            rb2d.velocity = new Vector2(moveSpeed, 0);
-            transform.localScale = new Vector2(1, 1);
-
-        } 
-        else  
-        {
-            rb2d.velocity = new Vector2(-moveSpeed, 0);
-            transform.localScale = new Vector2(-1, 1);
-
-        }
-
-    }
-
-    void StopChasingPlayer()
-    {
-        rb2d.velocity = new Vector2(0, 0);
     }
 
 }
